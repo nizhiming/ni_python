d47c3f422b0b701704f9b9fb5d118d25dda56852
nizhiming <ni_zhiming@outlook.com>
2017-06-27 17:29:55 +0800

    django模型文件——管理工具超级用户文件
    管理工具的操作配置文件

--- ni_python/ni_projectName/ni_Web/TestModel/admin.py
+++ ni_python/ni_projectName/ni_Web/TestModel/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from TestModel.models import Test,Contact,Tag
 
 # Register your models here.
+class TagInline(admin.TabularInline):
+    model = Tag
+    
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name','age','email')   #list 列表显示
+    search_fields = ('name',)
+    inlines = [TagInline]   # Inline 内联显示
+    #fields = ('name', 'email') # 自定义表单
+    fieldsets = (
+        ['Main',{
+            'fields':('name','email'),
+        }],
+        ['Advance',{
+            'classes':('collapse',),    # CSS
+            'fields': ('age',),
+        }]
+    )
+    
+admin.site.register(Contact, ContactAdmin)
+#admin.site.register([Test, Tag])
+admin.site.register([Test])
